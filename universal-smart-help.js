28af22cf86ee18fcff553cca28396a64ac50e82e
khansaid320-dev <khansaid320@gmail.com>
2026-08-18 06:58:27 +0200

    Delete universal-smart-help.js

--- universal-smart-help.js
+++
@@ -1,2 +0,0 @@
-/* Capability registry for the final universal assistant. */
-(function(){window.SaidSmartCapabilities={items:['multi-source web research','conversation context','travel planning','memory','reminders','calculator','weather','news','documents','images','CV','jobs','government information','transport information','app opening','Swedish/English','source ranking','concise answer formatting']};})();
